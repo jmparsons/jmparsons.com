a8ad4df19a432cf4317ac801b8ef85221d026c59
Jonathan Parsons <jonparsons@gmail.com>
2019-01-17 15:42:03 -0500

    Added router and pages.

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import Root from './containers/Root';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 serviceWorker.unregister();
