bc931cf7e03062c4355f339bd36df4943fbe66b7
Jonathan Parsons <jonparsons@gmail.com>
2019-01-20 18:19:25 -0500

    Separating theme objects.

--- src/constants/Theme.tsx
+++ src/constants/Theme.tsx
@@ -1,21 +1,22 @@
 import Colors from './Colors';
 import { ThemexProps } from '../interfaces';
 
-const Theme: ThemexProps = {
-  light: {
-    topBar: {
-      logo: Colors.darkSteel,
-      bg: Colors.offWhite,
-      link: Colors.darkBlueGray,
-    },
-  },
-  dark: {
-    topBar: {
-      logo: Colors.offWhite,
-      bg: Colors.darkBlueGray,
-      link: Colors.lightGray,
-    },
+const light = {
+  topBar: {
+    logo: Colors.darkSteel,
+    bg: Colors.offWhite,
+    link: Colors.darkBlueGray,
   },
 };
+
+const dark = {
+  topBar: {
+    logo: Colors.offWhite,
+    bg: Colors.darkBlueGray,
+    link: Colors.lightGray,
+  },
+};
+
+const Theme: ThemexProps = { light, dark };
 
 export default Theme;
