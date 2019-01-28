d7d2d76aec3823ad4330d0ded2f696bcab28935f
Jonathan Parsons <jonparsons@gmail.com>
2019-01-27 22:11:28 -0500

    Moved fonts to public, updated paths.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="react-scripts" />
+
+declare module '*.eot' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.woff' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ttf' {
+  const src: string;
+  export default src;
+}
