207c3770e6ad28d18e23090a40b3585a29b01e75
Jonathan Parsons <jonparsons@gmail.com>
2019-01-27 22:12:35 -0500

    Removed extra interfaces.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,16 +1,1 @@
 /// <reference types="react-scripts" />
-
-declare module '*.eot' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.woff' {
-  const src: string;
-  export default src;
-}
-
-declare module '*.ttf' {
-  const src: string;
-  export default src;
-}
