8821472d7acbcfeee400ab7d28748e2e596c1177
Jonathan Parsons <jonparsons@gmail.com>
2019-02-15 15:08:42 -0500

    Cleaning up topbar.

--- src/components/Site/TopBar.tsx
+++ src/components/Site/TopBar.tsx
@@ -1,30 +1,24 @@
 import React, { useContext } from 'react';
 import Routes from '../../constants/Routes';
 import { TopBarCon, NavBar, Logo, LogoLink, TopBarGrid, NxLink, HomeLink } from '../UI/TopBarUI';
-import { ThemerContext } from '../../utils/contexts';
-import Images from '../../constants/Images';
 
-const TopBar: React.FC = () => {
-  const { theme, toggleTheme } = useContext(ThemerContext);
-  const themeIcon = theme === 'light' ? Images.moon : Images.sun;
-  return (
-    <TopBarCon>
-      <TopBarGrid>
-        <Logo>
-          <LogoLink to={Routes.main}>JMParsons</LogoLink>
-        </Logo>
-        <NavBar>
-          <HomeLink to={Routes.main} exact>
-            Home
-          </HomeLink>
-          <NxLink to={Routes.services}>Services</NxLink>
-          <NxLink to={Routes.clients}>Clients</NxLink>
-          <NxLink to={Routes.about}>About</NxLink>
-          <NxLink to={Routes.contact}>Contact</NxLink>
-        </NavBar>
-      </TopBarGrid>
-    </TopBarCon>
-  );
-};
+const TopBar: React.FC = () => (
+  <TopBarCon>
+    <TopBarGrid>
+      <Logo>
+        <LogoLink to={Routes.main}>JMParsons</LogoLink>
+      </Logo>
+      <NavBar>
+        <HomeLink to={Routes.main} exact>
+          Home
+        </HomeLink>
+        <NxLink to={Routes.services}>Services</NxLink>
+        <NxLink to={Routes.clients}>Clients</NxLink>
+        <NxLink to={Routes.about}>About</NxLink>
+        <NxLink to={Routes.contact}>Contact</NxLink>
+      </NavBar>
+    </TopBarGrid>
+  </TopBarCon>
+);
 
 export default TopBar;
