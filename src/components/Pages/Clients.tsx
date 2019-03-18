273c14944c0e54c503abbdfc22f73a86ec7d49ba
Jonathan Parsons <jonparsons@gmail.com>
2019-03-18 18:42:17 -0400

    Added intersection observer support for clients.

--- src/components/Pages/Clients.tsx
+++ src/components/Pages/Clients.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import LayoutsMain from '../Layouts/Main';
 import clientData from '../../data/clients.json';
+import ClientImage from '../Site/ClientImage';
 import { ClientGrid, ClientItem } from '../UI/ClientsUI';
-import { cdn } from '../../utils/utils';
 
 const Clients: React.FC = () => (
   <LayoutsMain title="Clients · JMParsons">
@@ -10,7 +10,7 @@ const Clients: React.FC = () => (
     <ClientGrid>
       {clientData.clients.map(client => (
         <ClientItem key={client.name}>
-          <img src={cdn(client.image)} alt={client.name} />
+          <ClientImage name={client.name} image={client.image} />
         </ClientItem>
       ))}
     </ClientGrid>
--- src/components/UI/MainUI.tsx
+++ src/components/UI/MainUI.tsx
@@ -6,7 +6,8 @@ const MainGrid = styled.div`
   display: grid;
   background: ${({ theme }: ThxProps) => theme.main.bg};
   grid-template-rows: auto 1fr auto;
-  min-height: 100%;
+  width: 100%;
+  height: 100%;
   color: ${({ theme }: ThxProps) => theme.main.text};
 `;
 
