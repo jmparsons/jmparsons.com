65a94f318b6b3faec93c4c97f9ec6b88879f8edb
Jonathan Parsons <jonparsons@gmail.com>
2019-03-21 19:23:11 -0400

    Added google fonts with gatsby plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Open Sans`,
+            variants: [`300`, `400`, `700`],
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
