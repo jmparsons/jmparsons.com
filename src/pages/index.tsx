ec05b0471b794393ef0e0b95d925b6f988f006d6
Jonathan Parsons <jonparsons@gmail.com>
2019-03-22 03:49:12 -0400

    Testing theming in chrome issue.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Helmet from 'react-helmet';
 import LayoutsMain from '../components/Layout';
+import { ThemerContext } from '../utils/contexts';
 
-const Home: React.FC = () => (
-  <LayoutsMain>
-    <Helmet>
-      <meta
-        name="description"
-        content="Professional Scala, Play, Node, React, iOS, and Android Developer."
-      />
-    </Helmet>
-    <h1>Home</h1>
-    <p>Thanks for stopping by! Making some updates. They will be up shortly.</p>
-  </LayoutsMain>
-);
+const Home: React.FC = () => {
+  const { theme } = useContext(ThemerContext);
+  return (
+    <LayoutsMain>
+      <Helmet>
+        <meta
+          name="description"
+          content="Professional Scala, Play, Node, React, iOS, and Android Developer."
+        />
+      </Helmet>
+      <h1>Home</h1>
+      <p>curr theme: {theme}</p>
+      <p>Thanks for stopping by! Making some updates. They will be up shortly.</p>
+    </LayoutsMain>
+  );
+};
 
 export default Home;
