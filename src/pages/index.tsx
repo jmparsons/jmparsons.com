db86973a101a1467a76afb9322b17c2d19f3a16f
Jonathan Parsons <jonparsons@gmail.com>
2019-03-22 04:45:10 -0400

    Testing again.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,25 @@ import LayoutsMain from '../components/Layout';
 import { ThemerContext } from '../utils/contexts';
 
 const Home: React.FC = () => {
-  const { theme } = useContext(ThemerContext);
   return (
     <LayoutsMain>
-      <Helmet>
-        <meta
-          name="description"
-          content="Professional Scala, Play, Node, React, iOS, and Android Developer."
-        />
-      </Helmet>
-      <h1>Home</h1>
-      <p>curr theme: {theme}</p>
-      <p>Thanks for stopping by! Making some updates. They will be up shortly.</p>
+      <ThemerContext.Consumer>
+        {({ theme }) => {
+          return (
+            <>
+              <Helmet>
+                <meta
+                  name="description"
+                  content="Professional Scala, Play, Node, React, iOS, and Android Developer."
+                />
+              </Helmet>
+              <h1>Home</h1>
+              <p>curr theme: {theme}</p>
+              <p>Thanks for stopping by! Making some updates. They will be up shortly.</p>
+            </>
+          );
+        }}
+      </ThemerContext.Consumer>
     </LayoutsMain>
   );
 };
