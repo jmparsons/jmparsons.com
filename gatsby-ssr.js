2ba654aa474fac1c8678a2d9d9460fbf141b5c1d
Jonathan Parsons <jonparsons@gmail.com>
2019-03-25 03:42:31 -0400

    Testing again.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,22 +2,12 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
-// eslint-disable-next-line react/prop-types,react/display-name
-export const wrapRootElement = ({ element, pathname }) => {
-  const sheet = new ServerStyleSheet();
-  return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>;
-};
-
 export const onRenderBody = ({
   setHeadComponents,
   setHtmlAttributes,
   setBodyAttributes,
   pathname,
 }) => {
-  const sheet = new ServerStyleSheet();
-  if (sheet) {
-    setHeadComponents([sheet.getStyleElement()]);
-  }
   const helmet = Helmet.renderStatic();
   if (setHtmlAttributes) {
     setHtmlAttributes(helmet.htmlAttributes.toComponent());
@@ -33,4 +23,11 @@ export const onRenderBody = ({
     helmet.script.toComponent(),
     helmet.style.toComponent(),
   ]);
+};
+
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+  const sheet = new ServerStyleSheet();
+  const app = () => <StyleSheetManager sheet={sheet.instance}>{bodyComponent}</StyleSheetManager>;
+  replaceBodyHTMLString(renderToString(<app />));
+  setHeadComponents([sheet.getStyleElement()]);
 };
