7f809859cb866f8a1b606f5f5110f00959129104
Jonathan Parsons <jonparsons@gmail.com>
2019-03-25 03:01:48 -0400

    Testing ssr again.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,11 +1,35 @@
-// import React from 'react';
-// import { renderToString } from 'react-dom/server';
-// import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
-// export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
-//   const sheet = new ServerStyleSheet();
+const sheetByPathname = new Map();
 
-//   const app = () => <StyleSheetManager sheet={sheet.instance}>{bodyComponent}</StyleSheetManager>;
-//   replaceBodyHTMLString(renderToString(<app />));
-//   setHeadComponents([sheet.getStyleElement()]);
-// };
+// eslint-disable-next-line react/prop-types,react/display-name
+export const wrapRootElement = ({ element, pathname }) => {
+  const sheet = new ServerStyleSheet();
+  sheetByPathname.set(pathname, sheet);
+  return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>;
+};
+
+export const onRenderBody = ({ setHeadComponents, pathname }) => {
+  const sheet = sheetByPathname.get(pathname);
+  if (sheet) {
+    setHeadComponents([sheet.getStyleElement()]);
+    sheetByPathname.delete(pathname);
+  }
+  const helmet = Helmet.renderStatic();
+  if (setHtmlAttributes) {
+    setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  }
+  if (setBodyAttributes) {
+    setBodyAttributes(helmet.bodyAttributes.toComponent());
+  }
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ]);
+};
