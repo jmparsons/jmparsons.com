d544ec906a798eeb012a335dfa8662e3ee602271
Jonathan Parsons <jonparsons@gmail.com>
2019-03-25 03:39:57 -0400

    Testing full.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,40 +1,36 @@
-// import React from 'react';
-// import { Helmet } from 'react-helmet';
-// import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
-// const sheetByPathname = new Map();
+// eslint-disable-next-line react/prop-types,react/display-name
+export const wrapRootElement = ({ element, pathname }) => {
+  const sheet = new ServerStyleSheet();
+  return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>;
+};
 
-// // eslint-disable-next-line react/prop-types,react/display-name
-// export const wrapRootElement = ({ element, pathname }) => {
-//   const sheet = new ServerStyleSheet();
-//   sheetByPathname.set(pathname, sheet);
-//   return <StyleSheetManager sheet={sheet.instance}>{element}</StyleSheetManager>;
-// };
-
-// export const onRenderBody = ({
-//   setHeadComponents,
-//   setHtmlAttributes,
-//   setBodyAttributes,
-//   pathname,
-// }) => {
-//   const sheet = sheetByPathname.get(pathname);
-//   if (sheet) {
-//     setHeadComponents([sheet.getStyleElement()]);
-//     sheetByPathname.delete(pathname);
-//   }
-//   const helmet = Helmet.renderStatic();
-//   if (setHtmlAttributes) {
-//     setHtmlAttributes(helmet.htmlAttributes.toComponent());
-//   }
-//   if (setBodyAttributes) {
-//     setBodyAttributes(helmet.bodyAttributes.toComponent());
-//   }
-//   setHeadComponents([
-//     helmet.title.toComponent(),
-//     helmet.link.toComponent(),
-//     helmet.meta.toComponent(),
-//     helmet.noscript.toComponent(),
-//     helmet.script.toComponent(),
-//     helmet.style.toComponent(),
-//   ]);
-// };
+export const onRenderBody = ({
+  setHeadComponents,
+  setHtmlAttributes,
+  setBodyAttributes,
+  pathname,
+}) => {
+  const sheet = new ServerStyleSheet();
+  if (sheet) {
+    setHeadComponents([sheet.getStyleElement()]);
+  }
+  const helmet = Helmet.renderStatic();
+  if (setHtmlAttributes) {
+    setHtmlAttributes(helmet.htmlAttributes.toComponent());
+  }
+  if (setBodyAttributes) {
+    setBodyAttributes(helmet.bodyAttributes.toComponent());
+  }
+  setHeadComponents([
+    helmet.title.toComponent(),
+    helmet.link.toComponent(),
+    helmet.meta.toComponent(),
+    helmet.noscript.toComponent(),
+    helmet.script.toComponent(),
+    helmet.style.toComponent(),
+  ]);
+};
