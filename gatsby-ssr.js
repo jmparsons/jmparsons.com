8e5a633e6049ffe978d199ea342bb18c313fe2ba
Jonathan Parsons <jonparsons@gmail.com>
2019-03-25 02:47:03 -0400

    Testing non ssr.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+// import React from 'react';
+// import { renderToString } from 'react-dom/server';
+// import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
-export const replaceRenderer = ({
-  bodyComponent,
-  replaceBodyHTMLString,
-  setHeadComponents,
-}) => {
-  const sheet = new ServerStyleSheet();
+// export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+//   const sheet = new ServerStyleSheet();
 
-  const app = () => (
-    <StyleSheetManager sheet={sheet.instance}>
-      {bodyComponent}
-    </StyleSheetManager>
-  );
-  replaceBodyHTMLString(renderToString(<app />));
-  setHeadComponents([sheet.getStyleElement()]);
-};
+//   const app = () => <StyleSheetManager sheet={sheet.instance}>{bodyComponent}</StyleSheetManager>;
+//   replaceBodyHTMLString(renderToString(<app />));
+//   setHeadComponents([sheet.getStyleElement()]);
+// };
